5b77249bc02cb45b23ba756f22d5447ed5c8226f
MatiasMadoery <matimadoery@hotmail.com>
2025-02-04 12:48:06 -0300

    User views fixed

--- Control_Machine_Sistem/Models/User.cs
+++ Control_Machine_Sistem/Models/User.cs
@@ -1,8 +1,12 @@
-﻿namespace Control_Machine_Sistem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Control_Machine_Sistem.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
