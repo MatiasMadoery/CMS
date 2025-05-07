6bff26bf9d91fb35e8371f810f7de508a772f60f
MatiasMadoery <matimadoery@hotmail.com>
2025-05-07 08:04:37 -0300

    Categorie added at Machine create view

--- Control_Machine_Sistem/wwwroot/js/cleanNameFile.js
+++ Control_Machine_Sistem/wwwroot/js/cleanNameFile.js
@@ -2,7 +2,10 @@
     if (!url) return "";
 
     let fileName = url.split('/').pop();
+    console.log("cleanNameFile.js cargado correctamente!");
     let match = fileName.match(/_(.+)$/);
 
     return match ? match[1] : fileName;
+
+   
 }
